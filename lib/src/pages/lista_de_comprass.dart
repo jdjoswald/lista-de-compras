b3a844a2f64dc68e00c1439dec4f1f37663b5436
jdjoswald <joswaldleonardo07@gmail.com>
2020-05-19 08:51:13 -0400

    crearitem-swippe

--- lib/src/pages/lista_de_comprass.dart
+++ lib/src/pages/lista_de_comprass.dart
@@ -10,21 +10,20 @@ class ListaDeArticulos extends StatefulWidget {
 
 class _ListaDeArticulosState extends State<ListaDeArticulos> {
   bool comprado= false;
+  List<String> _poderes=["Dolar","Peso"];
+  String _opcionSeleccionada= "Peso";
+  String listName="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("lista inserte nombre"),
+        title: Text("lista $listName"),
       ),
 
       body: _lista() ,
 
       floatingActionButton: FloatingActionButton(
-       
-        onPressed:(){
-          Navigator.pushNamed(context,"crearobjeto");
-        } ,
-
+         onPressed: ()=> _crearitem(context,"","",0,""),
         child: Icon(Icons.add),
       ),
 
@@ -36,11 +35,9 @@ class _ListaDeArticulosState extends State<ListaDeArticulos> {
       return FutureBuilder(
         future:itemProvider.cargardata() ,
         builder: (context,AsyncSnapshot<List<dynamic>> snapshot ){
-
           return ListView(
               children: _listaItems(snapshot.data, context),
           );
-
         },
         initialData: [],
       );
@@ -49,67 +46,202 @@ class _ListaDeArticulosState extends State<ListaDeArticulos> {
     List<Widget>_listaItems(List<dynamic>data, BuildContext context) {
       final List<Widget> items=[];
       data.forEach((opt){
-        int precio=opt['precio'];
-        
-        final widgetTemp = CheckboxListTile(
-          
-          title: Row (
-            children: <Widget>[
-              Text(opt['articulo']),
-              Expanded(
-            flex: 2,
-            child: Container(
+      int precio=opt['precio'];
+        final widgetTemp = Dismissible(key: ObjectKey(opt),
+          onDismissed: (direction){
+            _crearitem(context,opt['articulo'], opt["link"],precio, opt["divisa"]);
+
+          },
+          background: Container(
+            color: Colors.red,
+            alignment: Alignment.topRight,
+            child:Column (
+             // mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Icon(Icons.delete)
+              ],
+            )
+          ),
+          secondaryBackground: Container(
+            color:Colors.green,
+            child: Icon(Icons.edit)
+          ),
+          child:CheckboxListTile( 
+            title: Row (
+              children: <Widget>[
+                Text(opt['articulo']),
+               Expanded(
+              flex: 2,
+              child: Container(
               height: 1,
             ),
           ),
               Text(" $precio ")],
           ),
           value:opt["estado"],
-          secondary: RaisedButton(onPressed:()=> Navigator.pushNamed(context,"crearobjeto"), child: Icon(Icons.edit,color:Colors.blue,)),
+         secondary: Column(
+           children: <Widget>[
+                
+                IconButton(onPressed:()=> _veritem(context,opt['articulo'], opt["link"],precio, opt["divisa"]),icon: Icon(Icons.remove_red_eye)),
+          ]
+           
+         ),
           onChanged: (valor){
              setState(() {
-
               comprado=valor;
              });  
           }, 
-        );
+        ));
         items..add(widgetTemp)
-                ..add(Divider());
-
-
-        
+                ..add(Divider());  
       });
       return items;
     }
-
-  void _editarBorrar(BuildContext context) {
+  _crearitem(BuildContext context, String nombre,String url,int costo, String divisa) {
     showDialog(
       context: context,
       barrierDismissible: true,
       builder: (context){
-
         return AlertDialog(
-          title: Text("editar borrar"),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+           title: Text("Crear item"),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
           content:Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.min, 
+            
             children: <Widget>[
-              Text("klk menol"),
-              FlutterLogo(size: 100.0,)
+              Container(
+                width:2000000.0,
+                height:2.0,
+                decoration: BoxDecoration(
+            color: Colors.white
+          ),
+              ),
+              
+               _crearinput(nombre,url,costo,divisa),
             ],
           ),
           actions: <Widget>[
             FlatButton(onPressed: null, child: Text("OK")),
             FlatButton(onPressed: ()=>Navigator.of(context).pop(), child: Text("Cancelar")),
-            // FlatButton(
-            //   onPressed: ()=>Navigator.of(context).pop(), 
-            
-            //   child: Text("si pero no")
-            // ),
-
           ],
+
+
         );
       }
     );
   }
+
+  Widget _crearinput( String nombre,String url,int costo, String divisa) {
+
+    if (divisa==_poderes[0]){
+      _opcionSeleccionada=_poderes[0];
+
+    }
+
+    return Column(
+      children: <Widget>[
+        
+        TextField(
+          decoration: InputDecoration(
+            labelText: "Nombre",
+            helperText: "Escribe el nombre del producto que quieres crear",
+            suffixIcon: Icon(Icons.list),
+            border:OutlineInputBorder(
+             borderRadius: BorderRadius.circular(20.0)
+            )
+          ),
+         ),
+
+         Divider(),
+         TextField(
+          keyboardType: TextInputType.url,
+          decoration: InputDecoration(
+            labelText: "url",
+            helperText: "Escribe el Link del lugar donde compraras el Articulo",
+            suffixIcon: Icon(Icons.place),
+            border:OutlineInputBorder(
+             borderRadius: BorderRadius.circular(20.0)
+            )
+          ),
+         ),
+
+         Divider(),
+         TextField(
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            labelText: "Costo",
+            helperText: "cuanto cuesta el articulo",
+            suffixIcon: Icon(Icons.attach_money),
+            border:OutlineInputBorder(
+             borderRadius: BorderRadius.circular(20.0)
+            )
+          ),
+         ),
+
+         Divider(),
+         DropdownButtonFormField(
+           value: _opcionSeleccionada,
+           items: getOpcionnesDropdwn(),  
+            onChanged: (opt){
+            setState(() {
+              _opcionSeleccionada=opt;
+              });
+            },
+           decoration: InputDecoration(
+            labelText: "Divisa",
+            helperText: "En que divisa esta el articulo recuerda cambiar el valor de divisas",
+            suffixIcon: Icon(Icons.attach_money),
+            border:OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0)
+            )
+           ),
+           
+           
+         ),
+      ],
+    );
+    
+  }
+  List<DropdownMenuItem<String>> getOpcionnesDropdwn(){
+    List<DropdownMenuItem<String>>lista = new List();
+    _poderes.forEach((poder){
+      lista.add(DropdownMenuItem(
+        child: Text(poder),
+        value: poder,
+      ));
+
+    });
+    return lista;
+  }
+
+  _veritem(BuildContext context, String nombre,String url,int costo, String divisa) {
+    showDialog(
+      context: context,
+       barrierDismissible: true,
+      builder: (context){
+         return AlertDialog(
+           title: Text(nombre),
+           shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          content:Column(
+             children: <Widget>[
+               Text(url),
+               Text(divisa),
+               Text(costo.toString()),
+              
+             ]
+            
+          ),
+          actions: <Widget>[
+            // FlatButton(onPressed: null, child: Text("OK")),
+            FlatButton(onPressed: ()=>Navigator.of(context).pop(), child: Text("OK")),
+          ],
+
+         );
+      }
+    );
+
+  }
+
+
+  
 }
